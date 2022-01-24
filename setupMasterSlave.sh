97b3d641caccbce6abbc25b8146d202d5b178ebb
David Collom <david@collom.co.uk>
2022-01-24 11:52:51 +0000

    Adding TLS Support and shellchecking all .sh (#8)
    Signed-off-by: David Collom <david.collom@jetstack.io>

--- setupMasterSlave.sh
+++ setupMasterSlave.sh
@@ -6,14 +6,14 @@ redis_server_mode() {
         if [[ -z "${REDIS_PASSWORD}" ]]; then
              redis-cli --cluster create "${MASTER_LIST}" --cluster-yes
         else
-            redis-cli --cluster create ${MASTER_LIST} --cluster-yes -a "${REDIS_PASSWORD}"
-        fi 
+            redis-cli --cluster create "${MASTER_LIST}" --cluster-yes -a "${REDIS_PASSWORD}"
+        fi
     elif [[ "${SERVER_MODE}" == "slave" ]]; then
         echo "Redis server mode is slave"
         if [[ -z "${REDIS_PASSWORD}" ]]; then
-            redis-cli --cluster add-node ${SLAVE_IP} ${MASTER_IP} --cluster-slave
+            redis-cli --cluster add-node "${SLAVE_IP}" "${MASTER_IP}" --cluster-slave
         else
-            redis-cli --cluster add-node ${SLAVE_IP} ${MASTER_IP} --cluster-slave -a "${REDIS_PASSWORD}"
+            redis-cli --cluster add-node "${SLAVE_IP}" "${MASTER_IP}" --cluster-slave -a "${REDIS_PASSWORD}"
         fi
     else
         echo "Redis server mode is standalone"
