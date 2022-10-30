730e3ce727b015d595e3e7f30105bcf0aa5d1503
Abhishek Dubey <abhishekbhardwaj510@gmail.com>
2022-10-30 22:44:29 +0530

    [Development][Add] Added ipv6 support in redis image (#17)
    * Added ipv6 support in redis image
    
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
    
    * Fixed healthcheck for ipv6 as well
    
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
    
    * Added ipv6 block in bind
    
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
    
    Signed-off-by: iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- healthcheck.sh
+++ healthcheck.sh
@@ -7,7 +7,7 @@ check_redis_health() {
     if [[ "${TLS_MODE}" == "true" ]]; then
         redis-cli --tls --cert "${REDIS_TLS_CERT}" --key "${REDIS_TLS_CERT_KEY}" --cacert "${REDIS_TLS_CA_KEY}" -h "$(hostname)" ping
     else
-        redis-cli ping
+        redis-cli -h $(hostname) ping
     fi
 }
 
