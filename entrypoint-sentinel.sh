0a838d925d54e191369aa2db9425b0badc0d0d45
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-01-04 20:16:26 +0530

    env variable can be set

--- entrypoint-sentinel.sh
+++ entrypoint-sentinel.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+#Redis Configuration needed
+PORT=${PORT:-6379}
+IP=${IP:-0.0.0.0}
+QUORUM=${QUORUM:-2}
+
+#Sentinel Config
 EXTERNAL_CONFIG_FILE=${EXTERNAL_CONFIG_FILE:-"/etc/redis/external.conf.d/redis-sentinel-additional.conf"}
+DOWN_AFTER_MILLISECONDS=${DOWN_AFTER_MILLISECONDS:-30000}
+PARALLEL_SYNCS=${PARALLEL_SYNCS:-1}
+FAILOVER_TIMEOUT=${FAILOVER_TIMEOUT:-180000}
+
+
+sentinel_mode_setup(){
+  {
+    echo "sentinel monitor ${MASTER_GROUP_NAME} ${IP} ${PORT} ${QUORUM}"
+    echo "sentinel down-after-milliseconds ${MASTER_GROUP_NAME} ${DOWN_AFTER_MILLISECONDS}"
+    echo "sentinel parallel-syncs ${MASTER_GROUP_NAME} ${PARALLEL_SYNCS}"
+    echo "sentinel failover-timeout ${MASTER_GROUP_NAME} ${FAILOVER_TIMEOUT}"
+  }>> /etc/redis/sentinel.conf
+ 
+}
 
 external_config() {
   echo "include ${EXTERNAL_CONFIG_FILE}" >> /etc/redis/sentinel.conf
@@ -14,6 +34,8 @@ start_sentinel() {
 }
 
 main_function() {
+
+  sentinel_mode_setup
 
   if [[ -f "${EXTERNAL_CONFIG_FILE}" ]]; then
     external_config
