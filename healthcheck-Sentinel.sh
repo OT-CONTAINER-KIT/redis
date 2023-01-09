dc5d98556471851dee1e7027c32cefef679133d6
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-01-10 02:11:11 +0530

    add Health Check

---
+++ healthcheck-Sentinel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+check_redis_health() {
+    if [[ -n "${REDIS_PASSWORD}" ]]; then
+        export REDISCLI_AUTH="${REDIS_PASSWORD}"
+    fi
+    if [[ "${TLS_MODE}" == "true" ]]; then
+        redis-cli --tls --cert "${REDIS_TLS_CERT}" --key "${REDIS_TLS_CERT_KEY}" --cacert "${REDIS_TLS_CA_KEY}" -h "$(hostname)" -p 26379 ping
+    else
+        redis-cli -h $(hostname) -p 26379 ping
+    fi
+}
+
+check_redis_health
